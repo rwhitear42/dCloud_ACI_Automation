3ccb6e7c564fa705b3ce16e4661e787a24f30653
rwhitear <rwhitear@cisco.com>
2019-07-01 10:30:12 +0100

    Sleep timer. Sometimes required due to APIC request throttling.

---
+++ postman_sleep_timer.js
@@ -0,0 +1,17 @@
+# Place this code is the pre-request-script tab within Postman.
+
+function foo() {
+    return (new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("done!");   // passing argument is optional, can just use resolve()
+        }, 20000)   // specify the delay time in ms here..
+    }))
+}
+
+async function waitForMe() {
+    await foo().then((val) => {
+        console.log(val);  // not required, you can just do an await without then
+    })
+}
+
+waitForMe();
